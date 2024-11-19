31ca12c866be01262901250cad36c97d82062dc1
Fran Barro <67473576+franbarro@users.noreply.github.com>
2024-11-19 11:54:34 +0100

    Update update-hipreport.sh

--- update-hipreport.sh
+++ update-hipreport.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sudo sed 's/--client-os"/--client-os-NOCHANGE/g' /usr/lib/openconnect/hipreport.sh > /usr/lib/openconnect/hipreport.sh 
-suod sed 's/CLIENTOS=Windows/CLIENTOS=Mac/g' /usr/lib/openconnect/hipreport.sh > /usr/lib/openconnect/hipreport.sh
+sudo sed 's/CLIENTOS=Windows/CLIENTOS=Mac/g' /usr/lib/openconnect/hipreport.sh > /usr/lib/openconnect/hipreport.sh
+
+sudo sed -i 's/openconnect-args=.*/openconnect-args=\/usr\/lib\/openconnect\/hipreport.sh/g' /etc/gpservice/gp.conf
